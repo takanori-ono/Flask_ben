bcabd2197e4fd9c1509071455d33865ecac52b7f
takanori-ono <takanori_ono@yahoo.co.jp>
2025-10-23 15:35:22 +0900

    fixed

--- flask1/Scripts/activate.bat
+++ flask1/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\uTO110\Documents\source\python\flask\flask1
+set VIRTUAL_ENV=G:\tmp\Flask_ben\flask1
 
 if not defined PROMPT set PROMPT=$P$G
 
