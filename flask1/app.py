86e3d09ff8de2a46c5b5cca42342e224c837d3bc
takanori-ono <takanori_ono@yahoo.co.jp>
2025-10-23 15:26:39 +0900

    fixed

--- flask1/app.py
+++ flask1/app.py
@@ -7,9 +7,21 @@ def hello():
     return render_template("index.html")
     #return "こんにちは、Flaskの世界へ！"
 
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
 @app.route("/about")
 def about():
     return render_template("sidebar.html")
+
+@app.route("/second")
+def second():
+    return render_template("second.html")
+
+@app.route("/third")
+def third():
+    return render_template("third.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
