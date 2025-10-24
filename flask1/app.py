8f75efec569212fc34f3abd8843e74fc69010644
takanori-ono <takanori_ono@yahoo.co.jp>
2025-10-24 15:00:28 +0900

    フォーム・DBの追加

--- flask1/app.py
+++ flask1/app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 
+# SQLiteデータベースの設定
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f'<Id {self.id}><User {self.name}>'
+
+with app.app_context():
+    db.create_all()
+    
 @app.route("/")
 def hello():
     return render_template("index.html")
@@ -22,6 +39,28 @@ def second():
 @app.route("/third")
 def third():
     return render_template("third.html")
+
+@app.route("/formtest", methods=["GET", "POST"])
+def formtest():
+    if request.method == "POST":
+        # name = request.form.get("name")
+        # return f"こんにちは、{name}さん！フォームの送信が成功しました。"
+        name = request.form.get("name")
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("result", name=name))
+    else:
+        return render_template("form.html")
+
+@app.route("/result/<name>")
+def result(name):
+    return render_template("result.html", name=name)
+
+@app.route("/users")
+def users():
+    all_users = User.query.all()
+    return render_template("users.html", users=all_users)
 
 if __name__ == "__main__":
     app.run(debug=True)
